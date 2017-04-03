4dbec2f78771ee2274aafc43f43b09a088a08742
Leonid Nikolaev <nikonor@nikonor.ru>
2017-04-03 12:29:56 +0300

    new file:   umb_lib.go 	new file:   umb_lib_test.go

---
+++ umb_lib.go
@@ -0,0 +1,136 @@
+package umb_lib
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    "strings"
+    "encoding/json"
+    "time"
+    "reflect"
+)
+
+const (
+    default_filename = "./config.txt"
+)
+
+
+func ToPrettyNameForm (s string) string {
+    return strings.Title(strings.ToLower(s))
+} 
+
+// Конвертируем привычную дату в time.Time
+func D2T (d string) (time.Time,error) {
+    return time.Parse("2.1.2006",d)    
+}
+
+// Конвертируем time.Time в привычную дату
+func T2D (d time.Time) (string) {
+    return d.Format("02.01.2006")    
+}
+
+
+// читаем конфиг. По умолчанию default_filename="/home/www/numb/cgi-bin/main/config.txt"
+func ReadConf( filename string ) map[string]string {
+    Config := make(map[string]string)
+
+    if filename == "" {
+        filename = default_filename
+    }
+
+    data,err := ioutil.ReadFile(filename)
+    Check_err(err,1)
+
+    var rows = strings.Split(string(data),"\n")
+    for i := 0; i < len(rows); i++ {
+        if strings.HasPrefix(rows[i],"#") != true && strings.Index(rows[i],"=") != -1 {
+            addConf(Config,rows[i],"")
+        }
+    }
+    return Config
+}
+
+// Получаем конфиг для отправки почты 
+func GetEMailConf (conf map[string] string, key string) (ret map[string]string,err error) {
+
+    err = json.Unmarshal([]byte(conf[strings.ToUpper(key)]),&ret)
+
+    return ret,err
+}
+
+
+
+// парсинг данный для SMTP из конфига
+func ParseConfEmail ( data string ) map[string]string {
+    smtp_server_conf := make(map[string]string)
+    err := json.Unmarshal([]byte(data),&smtp_server_conf)
+    Check_err(err,1)
+
+    return smtp_server_conf
+}
+
+func addConf (C map[string]string, row string, suffix string ) {
+    parts := strings.SplitN(row,"=",2)
+    if parts[0] != "" && parts[1] != "" {
+        if strings.HasPrefix(parts[0],"DBNAME") {
+            // особый случай DBNAME/DBNAME2
+            subparts := strings.Split(parts[1],";")
+            C[strings.ToUpper(parts[0])+suffix] = subparts[0]
+            s := ""
+            if parts[0] == "DBNAME2" {
+                s = "2"
+            }
+            for i := 1; i < len(subparts); i++ {
+                addConf(C,subparts[i],s)
+            }
+        } else {
+            C[strings.ToUpper(parts[0])+suffix] = parts[1]
+        }
+    }
+}
+
+// Функция для обработки ошибок. Если t=1, то panic, иначе просто выводим сообщение об ошибке
+func Check_err(e error, t int64) {
+    if e != nil {
+        if t == 1 {
+            panic(e)
+        } else {
+            fmt.Fprintf(os.Stderr,"Error: %s!",e)
+        }
+    }
+}
+
+func Check_errs(errs []error, t int64) {
+    if len(errs) > 0 {
+        fmt.Fprintf(os.Stderr,"Error:");
+        for _,e := range errs {
+            fmt.Fprintf(os.Stderr,"\t%s\n",e);
+        }
+        if t == 1 {
+            panic("Error")
+        }
+    }
+}
+
+func Round(val float64, places int ) (float64) {
+    var pow float64 = 1
+    for i:=0; i<places; i++ {
+        pow *= 10
+    }
+    return float64(int((val * pow) + 0.5)) / pow    
+}
+
+// Получени значения из структуры по имени
+// пример https://gist.github.com/nikonor/2eebddb68f8925c2e8b0ed405e5bb8da
+func GetValueByName(v interface{}, field string) interface{} {
+    r := reflect.ValueOf(v)
+    f := reflect.Indirect(r).FieldByName(field)
+    return f
+}
+
+// Установка значения поля структуры по имени
+// пример https://gist.github.com/nikonor/2eebddb68f8925c2e8b0ed405e5bb8da
+func SetValueByName(v interface{}, field string, newval interface{}) {
+    r := reflect.ValueOf(v).Elem().FieldByName(field)
+    r.Set(reflect.ValueOf(newval))
+}
---
+++ umb_lib_test.go
@@ -0,0 +1,61 @@
+package umb_lib
+
+import (
+    "fmt"
+    "testing"
+    "time"
+)
+
+func TestReadConf(t *testing.T) {
+    conf := ReadConf("")
+
+    cases := []struct{ key, want string }{
+                {"DBUSER", "tamaex"},
+                {"QWE", ""},
+                {"", ""},
+        }
+
+    for _, c := range cases {
+        fmt.Println("key=",c.key,", got=",conf[c.key],", want=",c.want);
+        if conf[c.key] != c.want {
+            t.Errorf("Error on ReadConf: want=%s, got=%s\n", c.want, conf[c.key])
+        }
+    }
+}
+
+func TestTimeConv(t *testing.T) {
+    cases := []struct{tt time.Time; ts string}{
+        {time.Date(2010,time.June,13,0,0,0,0,time.UTC),"13.06.2010"},
+    }
+
+    for _,c := range cases {
+        got := T2D(c.tt)
+        fmt.Printf("tt=%#v, ts=%s, got=%s\n",c.tt,c.ts,got)
+        if got != c.ts {
+            t.Errorf("Error on T2D: want=%s, got=%s\n",c.ts,got)
+        }
+        tt2,err := D2T(c.ts)
+        fmt.Printf("tt=%#v, tt2=%#v\n",c.tt,tt2)
+        if tt2 != c.tt || err != nil {
+            t.Errorf("Error on D2T: \twant=%#v, got=%#v\n\terr=%s\n",c.tt,tt2,err)
+        }
+    }
+}
+
+func TestRound(t *testing.T) {
+    cases := []struct{in1 float64;in2 int; want float64}{
+        {1.234,2,1.23},
+        {1.236,2,1.24},
+        {1.234,0,1},
+        {0,0,0},
+    }
+
+    for _,c := range cases {
+        got := Round(c.in1,c.in2)
+        fmt.Printf("Error on Round(%f,%d)=%f <?> %f\n",c.in1,c.in2,got,c.want)
+        if got != c.want {
+            t.Errorf("Error on Round(%f,%d)=%f != %f\n",c.in1,c.in2,got,c.want)
+        }
+    }
+}
+
